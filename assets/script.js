10241a09b5e014f8940a3a76ee1e4f64ebae5d68
haleyelder <haley.elder@live.com>
2021-05-15 20:27:20 -0700

    add learn in public page, other css

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var xhr = new XMLHttpRequest();
 xhr.onload = function () {
 	if (xhr.status >= 200 && xhr.status < 300) {
         let response = JSON.parse(xhr.response)
-        console.log(response)
+        //console.log(response)
     
         let recentPosts = document.getElementById("recent-posts");
         let recentPostLength = response.length;
